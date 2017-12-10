485deaafc96f9de494b14090bc474f9de9fc8fd6
s0ph1e <sophia@debitoor.com>
2017-12-10 23:18:27 +0200

    Add server

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+const cookieName = 'gh_access_token';
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+passport.use(new GitHubStrategy({
+		clientID: process.env.GITHUB_APP_ID,
+		clientSecret: process.env.GITHUB_APP_SECRET
+	},
+	(accessToken, refreshToken, profile, done) => {
+		process.nextTick(() => done(null, {accessToken, profile}));
+	}
+));
 
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
+app.use(passport.initialize());
+app.use(express.static('build'));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('build'));
-}
+app.get('/auth/github', passport.authenticate('github', {}));
 
-app.listen(app.get('port'), () => {
-	console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+app.get('/auth/github/callback',
+	passport.authenticate('github', {}),
+	(req, res) => {
+		res.cookie(cookieName, req.user.accessToken);
+		res.redirect('/')
+	}
+);
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
